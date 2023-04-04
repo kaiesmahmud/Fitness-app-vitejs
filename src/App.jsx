36efd421098c932bdf9269c85de688d3bca40778
kaiesmahmud <kaiesmahmud@gmail.com>
2023-04-04 12:21:08 +0600

    basic layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { Button } from '@mui/material/Button'
+import { Route, Routes} from 'react-router-dom'
+//Material UI
+import Button from '@mui/material/Button';
+import {Box} from '@mui/material'
+//css
 import './App.css'
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+//Pages
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
 
-function App() {
+const App = () => {
 
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <Box width="400px" sx={{ width: { xl : '1488px'}}} m={"auto"}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/exercise/:id' element={<ExerciseDetails/>} />
+      </Routes>
+      <Footer/>
+    </Box>
   )
 }
 
