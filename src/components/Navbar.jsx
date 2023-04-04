671150c465d5ba519e2a608105e2584925905c1b
kaiesmahmud <kaiesmahmud@gmail.com>
2023-04-04 13:53:44 +0600

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,28 @@ import Logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack
+     className=' capitalize'
+     direction={'row'}
+     justifyContent={"space-around"}
+     sx={{gap : { sm: '122px', xs: '40px',
+          mt:{ sm: '32px', xs: '20px'},
+          justifyContent: 'none'
+    }}}
+    px={'20px'}
+    >
         <Link to={'/'}>
             <img src={Logo} alt="Logo" className='w-[40px]' />
         </Link>
+        <Stack
+          direction={'row'}
+          gap={'40px'}
+          fontSize={"24px"}
+          alignItems={'flex-end'}
+        >
+          <Link to={'/'}>Home</Link>
+          <a href="#exercise" className='text-[#3a1212'>Exercises</a>
+        </Stack>
     </Stack>
   )
 }
