7f1dbbcd8186a7adcc5e114f5eb1ddc60d7e6038
kaiesmahmud <kaiesmahmud@gmail.com>
2023-04-04 13:57:29 +0600

    header, search, exercise components added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import Header from '../components/Header'
+import Search from '../components/Search'
+import Exercises from '../components/Exercises'
 
 const Home = () => {
   return (
-    <div>Home
-
-
+    <div>
+      <Header />
+      <Search />
+      <Exercises/>
     </div>
   )
 }
